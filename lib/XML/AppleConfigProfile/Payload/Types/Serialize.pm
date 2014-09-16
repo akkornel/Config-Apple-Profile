28a7890001b6b60ac10348607b0710beacaef927
A. Karl Kornel <karl@kornel.us>
2014-09-15 18:48:03 -0700

    Indenting was totally messed up

--- lib/XML/AppleConfigProfile/Payload/Types/Serialize.pm
+++ lib/XML/AppleConfigProfile/Payload/Types/Serialize.pm
@@ -66,92 +66,92 @@ An exception will be thrown if C<$type> or C<$subtype> are not recognized.
 sub serialize {
     my ($type, $value, $subtype) = @_;
     
-        # Strings need to be encoded as UTF-8 before export
-        if (   ($type == $ProfileString)
-            || ($type == $ProfileIdentifier)
-        ) {
-            $value = Mac::PropertyList::string->new(
-                Encode::encode('UTF-8', $value)
-            );
+    # Strings need to be encoded as UTF-8 before export
+    if (   ($type == $ProfileString)
+        || ($type == $ProfileIdentifier)
+    ) {
+        $value = Mac::PropertyList::string->new(
+            Encode::encode('UTF-8', $value)
+        );
+    }
+    
+    # Numbers are easy
+    elsif ($type == $ProfileNumber) {
+        $value = Mac::PropertyList::integer->new($value);
+    }
+    
+    # Reals are similarly easy, but use an uppercase E
+    elsif ($type == $ProfileReal) {
+        $value = Mac::PropertyList::real->new(uc($value));
+    }
+    
+    # All data is Base64-encoded for us by Mac::PropertyList
+    elsif (   ($type == $ProfileData)
+           || ($type == $ProfileNSDataBlob)
+    ) {
+        $value = Mac::PropertyList::data->new($value);
+    }
+    
+    # There are separate objects for true/false booleans
+    elsif ($type == $ProfileBool) {
+        if ($value) {
+            $value = Mac::PropertyList::true->new;
         }
-        
-        # Numbers are easy
-        elsif ($type == $ProfileNumber) {
-            $value = Mac::PropertyList::integer->new($value);
-        }
-        
-        # Reals are similarly easy, but use an uppercase E
-        elsif ($type == $ProfileReal) {
-            $value = Mac::PropertyList::real->new(uc($value));
-        }
-        
-        # All data is Base64-encoded for us by Mac::PropertyList
-        elsif (   ($type == $ProfileData)
-               || ($type == $ProfileNSDataBlob)
-        ) {
-            $value = Mac::PropertyList::data->new($value);
-        }
-        
-        # There are separate objects for true/false booleans
-        elsif ($type == $ProfileBool) {
-            if ($value) {
-                $value = Mac::PropertyList::true->new;
-            }
-            else {
-                $value = Mac::PropertyList::false->new;
-            }
-        }
-        
-        # Date
-        elsif ($type == $ProfileDate) {
-            # Set the time zone to UTC, make a string, and plist that
-            $value->set_time_zone('UTC');
-            my $string = $value->ymd . 'T' . $value->hms . 'Z';
-            $value = Mac::PropertyList::date->new($string);
-        }
-        
-        # UUIDs are converted to strings, then processed as such
-        elsif ($type == $ProfileUUID) {
-            $value = Mac::PropertyList::string->new(
-                Encode::encode('UTF-8', $value->as_string())
-            );
-        }
-        
-        # For arrays, make a Perl array of fragments, then plist that
-        elsif ($type == $ProfileArray) {
-            my @array;
-            
-            # Go through each array item, serialize it, and add it to our array
-            foreach my $item (@$value) {
-                push @array, serialize($subtype, $item, undef);
-            }
-            
-            $value = Mac::PropertyList::array->new(\@array);
-        }
-        
-        # For hashed, make a Perl hash of fragments, then plist that
-        elsif ($type == $ProfileDict) {
-            my %hash;
-            
-            # Go through each hash key, serialize it, and add it to our array
-            foreach my $key (keys %$value) {
-                $hash{$key} = serialize($subtype, $value->{$key}, undef);
-            }
-            
-            $value = Mac::PropertyList::dict->new(\%hash);
-        }
-        
-        # For class, let it serialize itself
-        elsif ($type == $ProfileClass) {
-            return $value->plist;
-        }
-        
-        # We've checked all the types we know about
         else {
-            die "Unknown type $type";
+            $value = Mac::PropertyList::false->new;
+        }
+    }
+    
+    # Date
+    elsif ($type == $ProfileDate) {
+        # Set the time zone to UTC, make a string, and plist that
+        $value->set_time_zone('UTC');
+        my $string = $value->ymd . 'T' . $value->hms . 'Z';
+        $value = Mac::PropertyList::date->new($string);
+    }
+    
+    # UUIDs are converted to strings, then processed as such
+    elsif ($type == $ProfileUUID) {
+        $value = Mac::PropertyList::string->new(
+            Encode::encode('UTF-8', $value->as_string())
+        );
+    }
+    
+    # For arrays, make a Perl array of fragments, then plist that
+    elsif ($type == $ProfileArray) {
+        my @array;
+        
+        # Go through each array item, serialize it, and add it to our array
+        foreach my $item (@$value) {
+            push @array, serialize($subtype, $item, undef);
         }
         
-        return $value;
+        $value = Mac::PropertyList::array->new(\@array);
+    }
+    
+    # For hashed, make a Perl hash of fragments, then plist that
+    elsif ($type == $ProfileDict) {
+        my %hash;
+        
+        # Go through each hash key, serialize it, and add it to our array
+        foreach my $key (keys %$value) {
+            $hash{$key} = serialize($subtype, $value->{$key}, undef);
+        }
+        
+        $value = Mac::PropertyList::dict->new(\%hash);
+    }
+    
+    # For class, let it serialize itself
+    elsif ($type == $ProfileClass) {
+        return $value->plist;
+    }
+    
+    # We've checked all the types we know about
+    else {
+        die "Unknown type $type";
+    }
+    
+    return $value;
 }
 
 
