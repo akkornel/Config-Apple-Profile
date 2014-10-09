f568c9ab02ac15815c39bb654e9446c1ebf57081
A. Karl Kornel <karl@kornel.us>
2014-10-09 01:51:44 -0700

    Defined our methods and wrote POD

--- lib/Config/Apple/Profile/Sign.pm
+++ lib/Config/Apple/Profile/Sign.pm
@@ -34,11 +34,8 @@ Config::Apple::Profile::Sign - Digitally sign configuration profiles
     
     my $signer = new Config::Apple::Profile::Sign;
     
-    $signer->set_key($signing_key, $password);
-    # Or $signer->set_key_path($signing_key_path, $password)
-    
-    $signer->set_cert($signing_cert);
-    # Or $signer->set_cert_path($signing_cert_path)
+    $signer->set_signer($signing_cert, $signing_key, $password);
+    # Or $signer->set_signer_path($cert_path, $key_path, $password)
     
     my $signed_profile = $signer->sign($profile);
 
@@ -148,6 +145,95 @@ sub new {
 
 
 =head1 INSTANCE METHODS
+
+
+=head2 set_signer($signing_cert, $signing_key, $password)
+
+Set the private key and certificate to be used for signing.
+
+C<$signing_cert> is the X.509 certificate, in DER or PEM form, provided as a
+string.  C<$signing_key> is the corresponding RSA private key, either in DER or
+PEM form, also provided as a string.  C<$password> is the password used to
+decrypt the private key.  If the private key is not encrypted, C<$password>
+should be an empty string.
+
+B<NOTE:> Storing the private key unencrypted is unsafe.
+ 
+This method (or C<set_signer_path>) must be called before the C<sign>
+method is called.  This method only needs to be called once, and then C<sign>
+can be called multiple times.
+
+Exceptions will be thrown if the certificate or key can not be read, or if the
+password provided can not be used to decrypt the key.  If exceptions are caught,
+but the program continues running, the instance which threw
+the exception will be in an inconsistent state, and should not be used for
+signing until C<set_signer> returns without errors.
+
+=cut
+
+sub set_signer {
+    my ($self, $signing_cert, $signing_key, $password) = @_;
+    
+    ...
+}
+
+
+=head2 set_signer_path($cert_path, $key_path, $password)
+
+This works the same as C<set_signer>, except the signing certificate and key
+are stored in files.
+
+The two files (C<$cert_path> and C<$key_path>) must both be readable.  They
+will not be modified.
+
+This method can die for the same reasons as C<set_signer>, and can also die if
+the files can not be read.
+
+=cut
+
+sub set_signer_path {
+    my ($self, $cert_path, $key_path, $password) = @_;
+    
+    ...
+}
+
+
+=head2 sign($profile)
+
+Sign a configuration profile.
+
+C<$profile> must be an instance of C<Config::Apple::Profile> which is ready for
+exporting.  A serialized configuration profile is obtained from C<$profile>,
+which is then signed.  The configuration profile, the signature, and a copy of
+the signing certificate are kept together in a DER-encoded PKCS#7 structure,
+which is returned to the client.
+
+Either C<set_signer()> or C<set_signer_path()> must be called before C<sign()>.
+
+The return value can be either an array or a scalar.  If a single scalar is
+returned, that will be the path to the signed data.  If an array is returned,
+the first entry in the array will be an open file handle, pointing to the start
+of the signed data, and the second entry will be the path to the file.  The file
+will be opened in C<binmode>.
+
+The signed profile is binary data, and must be treated as such.  The file's
+contents must not be modified in any way.  The client is responsible for
+unlinking the file.
+
+C<< $profile->export() >> will be called as part of the signing process, which
+means C<$profile> may be modified by C<sign()>.
+
+Exceptions will be thrown if signing credentials have not been provided, or if
+signing fails.  The call to C<< $profile->export() >> may also cause exceptions
+to be thrown.
+
+=cut
+
+sub sign {
+    my ($self, $profile) = @_;
+    
+    ...
+}
 
 
 =head2 DESTROY()
